822a490f596c9f6164e127aad03aaae220a760ac
LMPE AROS Muratbek Bulganbaev <m.bulganbaev@asbis.com>
2025-02-21 01:58:56 +0500

    yolo_test

--- processing/hailo_test.py
+++ processing/hailo_test.py
@@ -11,14 +11,14 @@ img = cv2.imread('data/images/test_image.jpg')
 # Обрабатываем изображение через модель
 detected = proc.process([img])
 
-# Отрисовываем боксы
+# Отрисовываем боксы (x1, y1, x2, y2)
 for result in detected:
     for box in result["absolute_boxes"]:
-        x1, y1, x2, y2 = box
-        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Зелёный цвет, толщина 2
+        y1, x1, y2, x2 = box  # Исправлено! Теперь порядок (x1, y1, x2, y2)
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Зелёный, толщина 2px
         cv2.putText(img, "Object", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-# Показываем изображение с бокcами
+# Показываем изображение с боксами
 cv2.namedWindow("Camera", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("Camera", 960, 540)
 cv2.imshow("Camera", img)
