8527f0a1e5d21f11872b1e2ad1d91753428cd103
LMPE AROS Muratbek Bulganbaev <m.bulganbaev@asbis.com>
2025-02-21 01:54:17 +0500

    yolo_test

--- processing/hailo_test.py
+++ processing/hailo_test.py
@@ -8,5 +8,10 @@ proc = hd.Processor(inf, 0.5)
 img = cv2.imread('data/images/test_image.jpg')
 detected = proc.process([img])
 # print(f'DETECTED {detected}')
+print(f'DETECTED {detected}')
+cv2.namedWindow("Camera", cv2.WINDOW_NORMAL)  # Делаем окно изменяемым
+cv2.resizeWindow("Camera", 960, 540)
+cv2.imshow("Camera", img)
+
 
 
