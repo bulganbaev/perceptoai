67f5cf01f675adba2c16fe852bf254a4da31fb6e
LMPE AROS Muratbek Bulganbaev <m.bulganbaev@asbis.com>
2025-02-26 20:23:26 +0500

    man

---
+++ dg/dg_test.py
@@ -0,0 +1,30 @@
+from cam.camera_driver import StereoCameraSystem
+import cv2
+import degirum as dg
+
+model = dg.load_model(
+    model_name='yolov8m_seg',
+    inference_host_address='@local',
+    zoo_url='/home/drone/git/model_zoo/yolov8m_seg/yolov8m_seg.json'
+)
+
+
+stereo = StereoCameraSystem()
+stereo.start()
+
+
+
+try:
+    while True:
+        frame_left, frame_right = stereo.get_synchronized_frames()
+        inference_result = model(frame_left)
+        cv2.imshow("Segmentation Output", inference_result.image_overlay)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+except KeyboardInterrupt:
+    print("⏹️ Остановка потока...")
+
+
+stereo.stop()
+cv2.destroyAllWindows()
+print("✅ Поток завершён.")
